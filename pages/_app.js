273832a8fae6e145995665346b9a83eb23032471
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-06-25 18:15:16 -0400

    scss login

--- pages/_app.js
+++ pages/_app.js
@@ -44,6 +44,7 @@ function MyApp({ Component, pageProps }) {
     <Head>
       {/* Font Awesome */}
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </Head>
     <socialContext.Provider value={{ user_id, setUser_id, users, mobile, router }}>
       <ProtectedLayout>
--- pages/login/index.js
+++ pages/login/index.js
@@ -55,140 +55,26 @@ const index = () => {
   }
 
   return loading ? <><Loading /></> : <>
-    <style>{`
-      .login-container {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 75px;
-      }
-
-      .wrapper {
-        position: relative;
-        margin-bottom: 50px;
-      }
-
-      .wrapper::after {
-        content: '';
-        position: absolute;
-        left: 0;
-        bottom: .07em;
-        height: .25em;
-        width: 100%;
-        background: linear-gradient(110deg, #e1f549, #29d0be, #6cb8ea, #ff5959);
-        z-index: -1;
-        transition: height .25s cubic-bezier(.6,0,.4,1);
-      }
-
-      .header {
-        font-family: 'IBM Plex Mono', monospace;
-        color: #FFF;
-        font-size: 10vw;
-      }
-
-      @media only screen and (min-width: 800px) {
-        .header {
-          font-size: 80px;
-        }
-      }
-
-      form div {
-        margin: 10px;
-      }
-      
-      form div input {
-          height: 45px;
-          width: 250px;
-          padding-left: 10px;
-          border-radius: 2px;
-          outline: none;
-          border: none;
-          background-color: rgb(64, 64, 82);
-          color: rgba(255, 255, 255, 0.8);
-      }
-      
-      form div input:hover {
-          background-color: rgb(72, 72, 90);
-      }
-      
-      input:-webkit-autofill,
-      input:-webkit-autofill:hover, 
-      input:-webkit-autofill:focus,
-      textarea:-webkit-autofill,
-      textarea:-webkit-autofill:hover,
-      textarea:-webkit-autofill:focus,
-      select:-webkit-autofill,
-      select:-webkit-autofill:hover,
-      select:-webkit-autofill:focus {
-        border: none;
-        -webkit-text-fill-color: rgba(255, 255, 255, 0.8);
-        -webkit-box-shadow: 0 0 0px 1000px rgb(72, 72, 90) inset;
-        transition: background-color 5000s ease-in-out 0s;
-      }
-      
-      .login-container div:nth-of-type(3) input {
-        width: 100%;
-        padding: 0;
-        color: #fff;
-        background-color: rgb(212, 84, 127);
-      }
-      
-      .login-container div:nth-of-type(3) input:hover {
-          cursor: pointer;
-          background-color: rgb(245, 97, 146);
-      }
-      
-      .register-container {
-        margin-top: 0!important;
-        width: calc(100% - 20px);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-      
-      .register-container-child {
-        margin: 0!important;
-        display: flex;
-        flex-direction: row;
-      }
-      
-      .register-container-child div {
-        margin: 0!important;
-        padding: 0 2.5px;
-        color: gray;
-      }
-      
-      a {
-        color: rgba(255, 255, 255, 0.8);
-        text-decoration: underline;
-      }
-    
-      a:hover {
-        color: rgba(245, 97, 146, 0.8);
-      }
-    `}</style>
     <Head>
       <title>Banned.Social | Login</title>
     </Head>
-    <div className="login-container" onSubmit={onSubmit}>
-      <div className="wrapper">
-        <div className="header no-select">banned.social</div>
+    <div className={styles.login_container}>
+      <div className={styles.login_header}>
+        <div className="no-select">banned.social</div>
       </div>
-      <form method="post">
-        <div>
+      <div className={`${styles.form_container} fade-in`}>
+        <form onSubmit={onSubmit}>
           <input ref={email} type="text" id="login" name="login" placeholder="Username" required />
-        </div>
-        <div>
           <input ref={password} type="password" id="password" name="password" placeholder="Password" required />
-        </div>
-        <div>
           <input type="submit" value="SIGN IN" />
-        </div>
-      </form>
-      <div className="register-container">
-        <div className="register-container-child">
-          <div>Not a member?</div><div><Link href="/register">Sign up now</Link></div>
+        </form>
+        <div className={styles.login_footer}>
+          <div>
+            <div>Not a member?</div>
+            <div>
+              <Link href="/register">Sign up now</Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
